1124808a2cb3f428b5288324a35353a37cedbf4c
Sergey Sekret <monkeyinsight@gmail.com>
2024-01-25 01:58:32 +0100

    add alacritty and fish

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim="nvim"
+alias firefox="/Applications/Firefox.app/Contents/MacOS/firefox"
+
+starship init fish | source
